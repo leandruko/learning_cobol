a8e8c51f322265a887f7c25c9f14f31a9712310c
Leandro Soto <124328946+leandruko@users.noreply.github.com>
2024-09-27 00:09:56 -0300

    update

--- learn.cbl
+++ learn.cbl
@@ -1,16 +1,21 @@
       ******************************************************************
-      * Author:
-      * Date:
-      * Purpose:
+      * Author: lea
+      * Date: 9/26/2024
+      * Purpose: learn
       * Tectonics: cobc
       ******************************************************************
        IDENTIFICATION DIVISION.
-       PROGRAM-ID. leandro-program.
+       PROGRAM-ID. lea-program.
+       AUTHOR. Leandro_Soto_Miranda.
+       INSTALLATION.
+
        DATA DIVISION.
        FILE SECTION.
        WORKING-STORAGE SECTION.
+
+
        PROCEDURE DIVISION.
        MAIN-PROCEDURE.
             DISPLAY "Hello world"
             STOP RUN.
-       END PROGRAM leandro-program.
+       END PROGRAM lea-program.
